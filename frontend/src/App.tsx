bdd7e1a1da9eafd7df7db33b688c49c5e29310aa
Paulo Ponte <gabeponte20@gmail.com>
2022-07-13 21:16:57 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import NotificationBtn from "./components/NotificationBtn";
+
 function App() {
-  return(
-    <h1>Olá Mundo</h1>
-  )
+  return (
+    <>
+      <h1>Olá Mundo</h1>
+      <NotificationBtn />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/NotificationBtn/index.tsx
@@ -0,0 +1,13 @@
+import icon from '../../assets/img/Vector.svg'
+
+import './styles.css'
+
+function NotificationBtn() {
+  return (
+    <div className="dsmeta-red-btn">
+      <img src={icon} alt="Notificar" />
+    </div>
+  );
+}
+
+export default NotificationBtn
